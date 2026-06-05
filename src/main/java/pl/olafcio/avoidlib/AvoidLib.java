8be4d0cec49d2ab1711583cb967f310468efa122
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-06-05 23:05:52 +0200

    Initial commit

---
+++ src/main/java/pl/olafcio/avoidlib/AvoidLib.java
@@ -0,0 +1,9 @@
+package pl.olafcio.avoidlib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AvoidLib implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/pl/olafcio/avoidlib/client/AvoidLibClient.java
@@ -0,0 +1,10 @@
+package pl.olafcio.avoidlib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AvoidLibClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
