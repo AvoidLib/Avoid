fc5d3d6b0df138df173d70c22e11befda67da3b7
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-06-12 23:03:10 +0200

    [Server Chat] Message send event

--- src/main/java/pl/olafcio/avoid/mixin/ServerPlayerMixin.java
+++ src/main/java/pl/olafcio/avoid/mixin/ServerPlayerMixin.java
@@ -8,13 +8,26 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import pl.olafcio.avoid.mods.event.EventManager;
 import pl.olafcio.avoid.net.chat.converter.COFromNative;
+import pl.olafcio.avoid.net.chat_server.event.ServerChatSendEvent;
 import pl.olafcio.avoid.net.chat_server.event.ServerChatSentEvent;
 import pl.olafcio.avoid.net.player.PlayerNative;
 
 @Mixin(ServerPlayer.class)
 public class ServerPlayerMixin {
-    @Inject(at = @At("TAIL"), method = "sendSystemMessage(Lnet/minecraft/network/chat/Component;Z)V")
+    @Inject(at = @At("TAIL"), method = "sendSystemMessage(Lnet/minecraft/network/chat/Component;Z)V", cancellable = true)
     public void sendSystemMessage(Component component, boolean bl, CallbackInfo ci) {
+        ServerChatSendEvent sendEvent = new ServerChatSendEvent(
+                PlayerNative.convertFrom((ServerPlayer) (Object) this),
+                COFromNative.from(component)
+        );
+
+        EventManager.fire(sendEvent);
+
+        if (sendEvent.isCancelled()) {
+            ci.cancel();
+            return;
+        }
+
         EventManager.fire(new ServerChatSentEvent(
                 PlayerNative.convertFrom((ServerPlayer) (Object) this),
                 COFromNative.from(component)
---
+++ src/main/java/pl/olafcio/avoid/net/chat_server/event/ServerChatSendEvent.java
@@ -0,0 +1,60 @@
+package pl.olafcio.avoid.net.chat_server.event;
+
+import org.jetbrains.annotations.NotNull;
+import pl.olafcio.avoid.net.chat.component.BaseComponent;
+import pl.olafcio.avoid.net.player.Player;
+
+import java.util.Objects;
+
+public final class ServerChatSendEvent {
+    private final @NotNull Player player;
+    private final @NotNull BaseComponent<?> message;
+    private boolean cancelled;
+
+    public ServerChatSendEvent(@NotNull Player player, @NotNull BaseComponent<?> message) {
+        this.player = player;
+        this.message = message;
+        this.cancelled = false;
+    }
+
+    public @NotNull Player player() {
+        return player;
+    }
+
+    public @NotNull BaseComponent<?> message() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ServerChatSendEvent) obj;
+        return Objects.equals(this.player, that.player) &&
+                Objects.equals(this.message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(player, message);
+    }
+
+    @Override
+    public String toString() {
+        return "ServerChatSendEvent[" +
+                "player=" + player + ", " +
+                "message=" + message + ']';
+    }
+
+    public void cancel() {
+        this.cancelled = true;
+    }
+
+    public void setCancelled(boolean value) {
+        this.cancelled = value;
+    }
+
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+}
