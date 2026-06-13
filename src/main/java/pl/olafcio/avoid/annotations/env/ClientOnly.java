18b0fce2909fdc5fe7290611ed22257f3f79d427
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-06-13 13:59:52 +0200

    [Documentation] @ServerOnly and @ClientOnly

--- src/main/java/pl/olafcio/avoid/annotations/env/ClientOnly.java
+++ src/main/java/pl/olafcio/avoid/annotations/env/ClientOnly.java
@@ -2,6 +2,10 @@ package pl.olafcio.avoid.annotations.env;
 
 import java.lang.annotation.*;
 
+/**
+ * Indicates a method only works on the object if it's created by the client,
+ * or if the object isn't associated, a client is running.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.CLASS)
 @Documented
--- src/main/java/pl/olafcio/avoid/annotations/env/ServerOnly.java
+++ src/main/java/pl/olafcio/avoid/annotations/env/ServerOnly.java
@@ -2,6 +2,10 @@ package pl.olafcio.avoid.annotations.env;
 
 import java.lang.annotation.*;
 
+/**
+ * Indicates a method only works on the object if it's created by a dedicated/integrated server,
+ * or if the object isn't associated, a dedicated or integrated server is running.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.CLASS)
 @Documented
