c18358f5237758a91f7fff18544e3c5f9590dc3c
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-13 15:02:58 +0200

    [MC 26.3] Delta tracking; @_noDeltaTracking

---
+++ src/main/java/pl/olafcio/avoid/net/entity_type/properties/_noDeltaTracking.java
@@ -0,0 +1,11 @@
+package pl.olafcio.avoid.net.entity_type.properties;
+
+import java.lang.annotation.*;
+
+/**
+ * <b>NOTE:</b> Only effective in Minecraft >=26.3.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface _noDeltaTracking {}
