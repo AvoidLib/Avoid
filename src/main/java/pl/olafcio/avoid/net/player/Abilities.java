06e87ff2911d0d33da955b96c85254e7fb173563
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-24 10:16:27 +0200

    [Documentation] All Abilities methods

--- src/main/java/pl/olafcio/avoid/net/player/Abilities.java
+++ src/main/java/pl/olafcio/avoid/net/player/Abilities.java
@@ -11,54 +11,90 @@ public final class Abilities {
         this.object = object;
     }
 
+    /**
+     * Returns whether the entity is flying.
+     */
     public boolean isFlying() {
         return object.flying;
     }
 
+    /**
+     * Sets the entity's flying state.
+     */
     public Abilities setFlying(boolean value) {
         object.flying = value;
         return this;
     }
 
+    /**
+     * Returns whether the entity is allowed to fly.
+     */
     public boolean isAllowedToFly() {
         return object.mayfly;
     }
 
+    /**
+     * Sets the entity's fly allowance state.
+     */
     public Abilities setAllowedToFly(boolean value) {
         object.mayfly = value;
         return this;
     }
 
+    /**
+     * Returns whether the entity is invulnerable.
+     */
     public boolean isInvulnerable() {
         return object.invulnerable;
     }
 
+    /**
+     * Sets the entity's invulnerability state.
+     */
     public Abilities setInvulnerable(boolean value) {
         object.invulnerable = value;
         return this;
     }
 
+    /**
+     * Returns whether the entity is allowed to build.
+     */
     public boolean isAllowedToBuild() {
         return object.mayBuild;
     }
 
+    /**
+     * Sets the entity's build allowance state.
+     */
     public Abilities setAllowedToBuild(boolean value) {
         object.mayBuild = value;
         return this;
     }
 
+    /**
+     * Returns the entity's fly speed.
+     */
     public float getFlySpeed() {
         return object.getFlyingSpeed();
     }
 
+    /**
+     * Sets the entity's fly speed.
+     */
     public void setFlySpeed(float value) {
         object.setFlyingSpeed(value);
     }
 
+    /**
+     * Returns the entity's walk speed.
+     */
     public float getWalkSpeed() {
         return object.getWalkingSpeed();
     }
 
+    /**
+     * Sets the entity's walk speed.
+     */
     public void setWalkSpeed(float value) {
         object.setWalkingSpeed(value);
     }
