95602d599ae01e0e2a6f73a5d00ab61c4ce5b7b6
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-08-19 18:24:08 +0200

    [Item Components] Transform MAP_DECORATIONS

---
+++ src/main/java/pl/olafcio/avoid/net/item/component/values/MapDecorations.java
@@ -0,0 +1,55 @@
+package pl.olafcio.avoid.net.item.component.values;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import pl.olafcio.avoid.net.id.Identification;
+import pl.olafcio.avoid.net.id.IdentificationNative;
+import pl.olafcio.avoid.net.item.component.TransformingItemComponentValue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record MapDecorations(Map<String, Entry> decorations) {
+    public record Entry(Identification type, double x, double z, float rotation) {}
+
+    public static final class Controller
+            implements TransformingItemComponentValue<net.minecraft.world.item.component.MapDecorations, MapDecorations>
+    {
+        @Override
+        public MapDecorations transform(net.minecraft.world.item.component.MapDecorations value) {
+            var src = value.decorations();
+            var dst = new HashMap<String, Entry>(src.size());
+
+            for (var entry : src.entrySet()) {
+                var key = entry.getKey();
+                var val = entry.getValue();
+
+                dst.put(key, new Entry(
+                        IdentificationNative.convertFrom(val.type().unwrapKey().orElseThrow().identifier()),
+                        val.x(), val.z(),
+                        val.rotation()
+                ));
+            }
+
+            return new MapDecorations(dst);
+        }
+
+        @Override
+        public net.minecraft.world.item.component.MapDecorations untransform(MapDecorations value) {
+            var src = value.decorations();
+            var dst = new HashMap<String, net.minecraft.world.item.component.MapDecorations.Entry>(src.size());
+
+            for (var entry : src.entrySet()) {
+                var key = entry.getKey();
+                var val = entry.getValue();
+
+                dst.put(key, new net.minecraft.world.item.component.MapDecorations.Entry(
+                        BuiltInRegistries.MAP_DECORATION_TYPE.wrapAsHolder(BuiltInRegistries.MAP_DECORATION_TYPE.get(IdentificationNative.convert(val.type())).orElseThrow().value()),
+                        val.x(), val.z(),
+                        val.rotation()
+                ));
+            }
+
+            return new net.minecraft.world.item.component.MapDecorations(dst);
+        }
+    }
+}
