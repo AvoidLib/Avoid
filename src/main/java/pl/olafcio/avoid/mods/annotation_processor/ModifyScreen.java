da39feb7b709f06812b20e948d97f3dd406bff79
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-09-05 17:23:57 +0200

    [Documentation] `@ModifyScreen`

--- src/main/java/pl/olafcio/avoid/mods/annotation_processor/ModifyScreen.java
+++ src/main/java/pl/olafcio/avoid/mods/annotation_processor/ModifyScreen.java
@@ -2,6 +2,7 @@ package pl.olafcio.avoid.mods.annotation_processor;
 
 import org.jetbrains.annotations.ApiStatus;
 import pl.olafcio.avoid.net.screen.ScreenMarker;
+import pl.olafcio.avoid.net.screen.Screens;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -9,6 +10,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Tells the modloader to automatically register the annotated method as a screen modifier for the screen type that
+ * matches the given marker, with the {@link Screens} namespace.
  * @see pl.olafcio.avoid.net.screen_modifier.ScreenModifier net.screen_modifier\ScreenModifier
  */
 @Retention(RetentionPolicy.RUNTIME)
