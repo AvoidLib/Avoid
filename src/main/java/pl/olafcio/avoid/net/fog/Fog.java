97fcd1898923b03d9929b63fec113dfebe548a4d
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-09-05 17:23:14 +0200

    [Documentation] Fog

--- src/main/java/pl/olafcio/avoid/net/fog/Fog.java
+++ src/main/java/pl/olafcio/avoid/net/fog/Fog.java
@@ -7,6 +7,25 @@ import pl.olafcio.avoid.net.fog.delta.TickTracker;
 import pl.olafcio.avoid.net.id.Identification;
 import pl.olafcio.avoid.net.world.World;
 
+/**
+ * A fog is a transparency effect.
+ * <br/><br/><hr/><br/>
+ * <b>Fogs can happen in very many situations:</b>
+ * <ul>
+ *     <li>with an effect,&emsp;&ensp; <i>(Nausea, Darkness)</i></li>
+ *     <li>inside a fluid,&emsp;&ensp;&nbsp;&nbsp; <i>(Lava, Water)</i></li>
+ *     <li>inside a block,&emsp;&ensp; <i>(Powdered Snow)</i></li>
+ *     <li>depending on where the sun is.</li>
+ * </ul>
+ * <br/><hr/><br/>
+ * <b>As a mod scenario of where you could add a fog - well, here's a few:</b>
+ * <br/>
+ * <ul>
+ *     <li>in  a horror scene,</li>
+ *     <li>for different biome sky colors,</li>
+ *     <li>for more scary caves.</li>
+ * </ul>
+ */
 @NullMarked
 public abstract class Fog {
     public int baseColor(World world, Entity entity, int renderDistance, float deltaTicks) {
