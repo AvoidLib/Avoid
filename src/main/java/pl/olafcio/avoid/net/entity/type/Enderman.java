f7e7e5a3ab4dbe6ac65b522affb1a33633a267fa
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-09-21 21:44:59 +0200

    Rename EnderMan to Enderman

---
+++ src/main/java/pl/olafcio/avoid/net/entity/type/Enderman.java
@@ -0,0 +1,30 @@
+package pl.olafcio.avoid.net.entity.type;
+
+import org.jetbrains.annotations.ApiStatus;
+import pl.olafcio.avoid.net.chat.component.BaseComponent;
+import pl.olafcio.avoid.net.entity.Entity;
+import pl.olafcio.avoid.net.entity.type.base.annotations.AutoEntityAttach;
+import pl.olafcio.avoid.net.entity.type.base.annotations.klass.Class;
+import pl.olafcio.avoid.net.entity_type.EntityType;
+import pl.olafcio.avoid.net.world.vect3.IVect3;
+
+import java.util.UUID;
+
+@AutoEntityAttach
+@Class(net.minecraft.world.entity.monster.EnderMan.class)
+
+@ApiStatus.Experimental
+
+public class Enderman extends Entity {
+    public Enderman(int id, EntityType type, IVect3 position, IVect3 velocity, UUID uuid, BaseComponent<?> name, net.minecraft.world.entity.Entity underlyingEntity) {
+        super(id, type, position, velocity, uuid, name, underlyingEntity);
+    }
+
+    public boolean isCreepy() {
+        return __cast(net.minecraft.world.entity.monster.EnderMan.class).isCreepy();
+    }
+
+    public boolean hasBeenStaredAt() {
+        return __cast(net.minecraft.world.entity.monster.EnderMan.class).hasBeenStaredAt();
+    }
+}
