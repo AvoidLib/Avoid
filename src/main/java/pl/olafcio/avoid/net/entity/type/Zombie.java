04ed56afe6f6142f7f4c403a4aa44576ef63cd73
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-09-21 21:01:57 +0200

    Fix MC 1.21.10

---
+++ src/main/java/pl/olafcio/avoid/net/entity/type/Zombie.java
@@ -0,0 +1,22 @@
+package pl.olafcio.avoid.net.entity.type;
+
+import org.jetbrains.annotations.ApiStatus;
+import pl.olafcio.avoid.net.chat.component.BaseComponent;
+import pl.olafcio.avoid.net.entity.Entity;
+import pl.olafcio.avoid.net.entity.type.base.annotations.AutoEntityAttach;
+import pl.olafcio.avoid.net.entity.type.base.annotations.klass.Class;
+import pl.olafcio.avoid.net.entity_type.EntityType;
+import pl.olafcio.avoid.net.world.vect3.IVect3;
+
+import java.util.UUID;
+
+@AutoEntityAttach
+@Class(net.minecraft.world.entity.monster.zombie.Zombie.class)
+
+@ApiStatus.Experimental
+
+public class Zombie extends Entity {
+    public Zombie(int id, EntityType type, IVect3 position, IVect3 velocity, UUID uuid, BaseComponent<?> name, net.minecraft.world.entity.Entity underlyingEntity) {
+        super(id, type, position, velocity, uuid, name, underlyingEntity);
+    }
+}
